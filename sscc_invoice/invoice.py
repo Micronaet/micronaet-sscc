8b18945a0bb7efc1abb4cbe58612029e0806cee5
gloria <gloria@dev04.micronaet.local>
2016-09-27 14:34:18 +0200

    STart import procedure

--- sscc_invoice/invoice.py
+++ sscc_invoice/invoice.py
@@ -61,14 +61,92 @@ class SsccInvoice  (orm.Model):
     _name = 'sscc.invoice'
     _description = 'SSCC Invoice'
     
+    # Import function:
+    def import_invoice_csv(self, cr, uid, ids, context=None):
+        ''' Import csv file
+        '''
+        # Pool used:
+        line_pool = self.pool.get('sscc.invoice.line')
+        
+        # Initial setup_
+        filename = '/home/thebrush/etl/fattura.csv' # TODO
+        
+        header = True
+        invoice_id = False
+        for line in open(filename, 'r'):
+            # -----------------------------------------------------------------     
+            # Header data:            
+            # -----------------------------------------------------------------     
+            if header:               
+                header = False                
+
+                # Fields:
+                number_invoice = line[0:6] # 6
+                date_invoice = line[6:16] # 10
+                partner_code = '' # TODO
+                
+                # Calculated fields:
+                partner_id = False # TODO
+               
+                # TODO check number invoice first
+                
+                # Create header
+                invoice_id = self.create(cr, uid, {
+                    'name': number_invoice,
+                    'date': date_invoice,
+                    'year': date_invoice[:4],                    
+                    'partner_id': partner_id,
+                    #'journal':
+                    }, context=context)
+                        
+            # -----------------------------------------------------------------     
+            # Row data:
+            # -----------------------------------------------------------------     
+            # Fields:
+            order_number = line[16:22] # 6
+            order_date = line[22:32] # 10
+            code = line[32:48] # 16
+            description = line[1:72]
+            uom = line[]
+            currency = line[]            
+            price = line[1:10] # 10
+            duty_code = line[] # 8
+            #sscc = line[] # 18
+            quantity = line[]
+            q_x_pack = line[]
+            parcel = line[]
+            net_weight = line[]
+            weight = line[]
+            lot = line[]
+            deadline = line[]
+            country_origin = line[]
+            country_from = line[] # Italy
+            #duty_ok = line[]
+            #mnr_number = line[]
+            #sanitary = line[]
+            #sanitary_date = line[]
+            #extra_code = line[]
+            #sif = line[]
+            
+            line_pool.create(cr, uid, {
+                'invoice_id': invoice_id,
+                'name': description,
+                'code': code,
+                'uom': uom,
+                'quantity': quantity,
+                'order_date': order_date,
+                'order_number': order_number,                 
+                 
+                 }, context=context)
+               
+        return True
+        
     _columns = {
         'name': fields.char('Number', size=15, required=True),
         'date': fields.date('Date'),
         'year': fields.char('Year', size=4),
-        'journal': fields.char('Journal', size=64, required=False, 
-            readonly=False),   
-        'partner_id': fields.many2one(
-            'res.partner', 'Partner'), 
+        'journal': fields.char('Journal', size=64),   
+        'partner_id': fields.many2one('res.partner', 'Partner'),         
         # Function complete code
         # TODO 1st char??    
         }
@@ -86,6 +164,11 @@ class SsccInvoiceLine  (orm.Model):
         'uom': fields.char('UOM', size=2), 
         'quantity': fields.float('Quantity', digits=(16, 2)), 
         'invoice_id': fields.many2one('sscc.invoice', 'Invoice'), 
+        'order_date': fields.date('Order date'),
+        'order_number': fields.char('Order number', size=15), 
+        'sscc_id': fields.many2one('sscc.code', 'SSCC code'), 
+        
+        
         # Function complete code
         # TODO 1st char??    
         }
@@ -95,12 +178,9 @@ class SsccInvoice  (orm.Model):
     """
     
     _inherit = 'sscc.invoice'
-
     
     _columns = {
-        'line_ids': fields.one2many(
-            'sscc.invoice.line', 'invoice_id', 
-            'Line'), 
+        'line_ids': fields.one2many('sscc.invoice.line', 'invoice_id', 'Line'), 
         }
 
         
