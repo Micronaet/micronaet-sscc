4b6ae1506b9ede0e63d4551deaad814dfda8326d
nicothebrush <nicola.riolini@gmail.com>
2016-10-05 14:49:47 +0200

    Add loop procedure and crop for generate file using online calc, start check barpy module

--- etl/get_sscc_code.py
+++ etl/get_sscc_code.py
@@ -17,30 +17,69 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-#from . import 
 import urllib2
+from PIL import Image
 
-url = 'http://barcode.tec-it.com/barcode.ashx?translate-esc=on&data=(00)180320890000000014&code=GS1_128CCA&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=000000&bgcolor=FFFFFF&qunit=Mm&quiet=0'
-file_name = 'prova.gif'
+def _sscc_check_digit(fixed):
+    ''' Generate check digit and return
+    '''
+    tot = 0
+    pos = 0    
+    for c in fixed:
+        pos+=1
+        number = int(c)
+        if pos % 2 == 0 :
+            tot += number
+        else:
+            tot += number*3
+    
+    remain = tot % 10
+    if remain:
+        return 10 - remain 
+    else: 
+        return 0
 
-u = urllib2.urlopen(url)
-f = open(file_name, 'wb')
-meta = u.info()
-file_size = int(meta.getheaders('Content-Length')[0])
-print 'Downloading: %s Bytes: %s' % (file_name, file_size)
+for i in range(10):#(1000000000):
+    sscc = '38004766%09d' % i
+    sscc = '%s%s' % (sscc, _sscc_check_digit(sscc))
+    
+    url = 'http://barcode.tec-it.com/barcode.ashx?translate-esc=on&data=(00)%s&code=GS1_128CCA&unit=Fit&dpi=96&imagetype=Gif&rotation=0&color=000000&bgcolor=FFFFFF&qunit=Mm&quiet=0' % sscc
+    file_name = '%s.gif' % sscc
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders('Content-Length')[0])
+    print 'Downloading: %s Bytes: %s' % (file_name, file_size)
+    
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+            
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (
+            file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print status,
+    f.close()
+    
+    # Crop image:
+    img = Image.open(file_name)
+    width = img.size[0]
+    height = img.size[1]
+    img2 = img.crop((0, 0, width, 127))
+    img2.save(file_name)
 
-file_size_dl = 0
-block_sz = 8192
-while True:
-    buffer = u.read(block_sz)
-    if not buffer:
-        break
-
-    file_size_dl += len(buffer)
-    f.write(buffer)
-    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-    status = status + chr(8)*(len(status)+1)
-    print status,
-
+'''
+import barpy
+a = barpy.GS1128('(00)380047660000000017', 'B', 'gs1')
+f = open('gs1.png', 'w')
+f = open('gs1.png', 'w')
+f.write(a.to_png())
 f.close()
+
+'''    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
